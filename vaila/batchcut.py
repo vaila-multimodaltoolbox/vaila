95e80dcd234a13eb9492c5547353060f484a99ba
vailá <vailamultimodaltoolbox@gmail.com>
2024-08-12 21:29:23 -0400

    upgrade drawboxe.py (#32)
    Co-authored-by: Paulo Santiago <paulosantiago@usp.br>

--- vaila/batchcut.py
+++ vaila/batchcut.py
@@ -1,3 +1,33 @@
+"""
+Batch Video Cutting Script
+Author: Prof. PhD. Paulo Santiago
+Date: August 12, 2024
+Version: 1.0
+
+Description:
+This script performs batch video cutting, processing a list of videos and saving the specified parts
+in an output directory. The video list should contain the original video name, the new name for the cut
+video, the start frame, and the end frame.
+
+List file format:
+<original_name> <new_name> <start_frame> <end_frame>
+
+Example:
+PC001_STS_02_FLIRsagital.avi PC001_STS_02_FLIRsagital_200_100_300.mp4 100 300
+
+The script creates a "cut_videos" subdirectory in the specified output directory, where the cut videos
+will be saved. If the new name already contains the .mp4 extension, it will be removed to avoid duplication.
+
+Dependencies:
+- ffmpeg-python
+- tkinter
+
+Usage:
+Run the script and follow the prompts to select the video directory, the list file, and the output
+directory.
+
+"""
+
 import os
 import sys
 from ffmpeg import FFmpeg
@@ -9,7 +39,7 @@ def batch_cut_videos(video_directory, list_file_path, output_directory):
         print(f"List file {list_file_path} does not exist.")
         return
     
-    # Cria o subdiretório "cut_videos" dentro do diretório de saída
+    # Create the "cut_videos" subdirectory inside the output directory
     output_directory = os.path.join(output_directory, "cut_videos")
     os.makedirs(output_directory, exist_ok=True)
     
@@ -29,6 +59,11 @@ def batch_cut_videos(video_directory, list_file_path, output_directory):
         start_frame, end_frame = int(start_frame), int(end_frame)
         
         original_path = os.path.join(video_directory, original_name)
+        
+        # Remove the duplicate .mp4 extension if it already exists in new_name
+        if new_name.endswith('.mp4'):
+            new_name = new_name[:-4]
+        
         new_path = os.path.join(output_directory, f"{new_name}.mp4")
         
         try:
