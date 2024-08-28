5c4445162c1a2757c674f8d5fc5a79f85f69caff
Paulo Santiago <paulosantiago@usp.br>
2024-08-27 23:08:27 -0400

    update yaml header and create install sh and bat

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         "matplotlib==3.8.4",
         "scipy==1.13.1",
         "dash==2.17.1",
-        "ffmpeg-python==0.2.0",
         "mediapipe==0.10.14",
         "opencv-contrib-python==4.10.0.84",
         "imufusion==1.2.5",
@@ -20,7 +19,7 @@ setup(
     author="Guilherme Cesar, Ligia Mochida, Bruno Bedo, Paulo Santiago",
     author_email="paulosantiago@usp.br",
     description="Versatile Anarcho Integrated Liberation Ánalysis in Multimodal Toolbox.",
-    url="https://github.com/paulopreto/vaila-multimodaltoolbox",
+    url="https://github.com/vaila-multimodaltoolbox/vaila",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
