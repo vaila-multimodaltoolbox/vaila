5c8aebe21b6ef97717766544bc4b23c8a8787589
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2024-12-03 06:12:30 -0300

    Calib dlt3 improve ffmpeg in codes video and png (#187)
    * dir test GRF gait
    
    * improve ffmpeg in codes video and png

--- vaila/compress_videos_h264.py
+++ vaila/compress_videos_h264.py
@@ -234,6 +234,9 @@ def run_compress_videos_h264(
 
 
 def compress_videos_h264_gui():
+    # Print the directory and name of the script being executed
+    print(f"Running script: {os.path.basename(__file__)}")
+    print(f"Script directory: {os.path.dirname(os.path.abspath(__file__))}")
     global success_count, failure_count
     root = Tk()
     root.withdraw()
