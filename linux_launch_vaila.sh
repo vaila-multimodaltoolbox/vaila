c9073aec6f3ed40b5196a86bb856bb6842bcf28f
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2025-05-15 13:27:58 -0300

    Mlgaitway (#317)
    * vailá click go to dir root of project
    
    * update ultralytics version 8.3.135
    
    * arxiv
    
    * update install linux all terminal
    
    * update launcher os

--- linux_launch_vaila.sh
+++ linux_launch_vaila.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Author: Paulo Santiago
+# Date: May 15, 2025
+# Updated: May 15, 2025
+# Version: 0.0.1
+# OS: Ubuntu, Kubuntu, Linux Mint, Pop_OS!, Zorin OS, etc.
+
 # Start Conda
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate vaila
--- mac_launch_vaila.sh
+++ mac_launch_vaila.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+
+# Author: Paulo Santiago
+# Date: May 15, 2025
+# Updated: May 15, 2025
+# Version: 0.0.1
+# OS: macOS
+
 # Start Conda
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate vaila
