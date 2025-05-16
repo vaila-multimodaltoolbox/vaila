2f0f21c3ac21fd8e6a660a810bdd88ddf5ef9e0f
paulopreto <paulosantiago@usp.br>
2025-05-16 16:48:07 -0300

    steps and features mlwalkway version vaila update

--- vaila.py
+++ vaila.py
@@ -4,8 +4,8 @@ vaila.py
 ===============================================================================
 Author: Paulo R. P. Santiago
 Date:  7 October 2024
-Update: 15 May 2025
-Version updated: 0.6.13
+Update: 16 May 2025
+Version updated: 0.6.14
 Python Version: 3.12.9
 
 Description:
@@ -133,7 +133,7 @@ if platform.system() == "Darwin":  # macOS
         pass
 
 text = r"""
-vailá - 15.May.2025 v0.6.13 (Python 3.12.9)
+vailá - 16.May.2025 v0.6.14 (Python 3.12.9)
                                              o
                                 _,  o |\  _,/
                           |  |_/ |  | |/ / |
@@ -212,7 +212,7 @@ class Vaila(tk.Tk):
 
         """
         super().__init__()
-        self.title("vailá - 15.May.2025 v0.6.13 (Python 3.12.9)")
+        self.title("vailá - 16.May.2025 v0.6.14 (Python 3.12.9)")
 
         # Adjust dimensions and layout based on the operating system
         self.set_dimensions_based_on_os()
