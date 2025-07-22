3a92336d03d51cdb5e7b59dae9ff5267d8e40d8c
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2025-07-21 22:04:02 -0300

    update markerless

--- vaila/filtering.py
+++ vaila/filtering.py
@@ -1,10 +1,46 @@
+"""
+Project: vailá Multimodal Toolbox
+Script: filtering.py - Filtering
+
+Author: Paulo Roberto Pereira Santiago
+Email: paulosantiago@usp.br
+GitHub: https://github.com/vaila-multimodaltoolbox/vaila
+Creation Date: 29 July 2025
+Update Date: 29 July 2025
+Version: 0.0.1
+
+Description:
+    Filtering.
+
+Usage:
+    Run the script from the command line:
+        python filtering.py
+
+Requirements:
+    - Python 3.x
+    - Scipy
+    - Numpy
+
+License:
+    This project is licensed under the terms of GNU General Public License v3.0.
+
+Change History:
+    - v0.0.1: First version
+"""
+
+import os
 import numpy as np
 from scipy.signal import butter, filtfilt, firwin
+from rich import print
 
 
 def apply_filter(
     data, sample_rate, method="butterworth", cutoff=5, order=4, numtaps=255, padlen=128
 ):
+    # Print the directory and name of the script being executed
+    print(f"Running script: {os.path.basename(__file__)}")
+    print(f"Script directory: {os.path.dirname(os.path.abspath(__file__))}")
+    
     """Apply a specified filter to the data with padding."""
     nyquist = 0.5 * sample_rate
     normal_cutoff = cutoff / nyquist
