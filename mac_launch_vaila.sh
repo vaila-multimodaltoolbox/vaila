d195a38cca97371500f26eb24cedac2d446fc0cc
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2025-07-23 10:16:08 -0300

    fix launcher and vila buttons OS (#370)

--- mac_launch_vaila.sh
+++ mac_launch_vaila.sh
@@ -1,12 +1,20 @@
-#!/bin/zsh
+#!/bin/bash  # (ou #!/bin/zsh para o mac)
 
 # Author: Paulo Santiago
 # Date: May 15, 2025
-# Updated: May 15, 2025
-# Version: 0.0.1
-# OS: macOS
+# Updated: July 23, 2025
+# Version: 0.0.2
+# OS: Ubuntu, Kubuntu, Linux Mint, Pop_OS!, Zorin OS, macOS, etc.
 
-# Start Conda
-source ~/anaconda3/etc/profile.d/conda.sh
+# Detect Conda installation (Anaconda or Miniconda)
+if [ -d "$HOME/anaconda3" ]; then
+    source "$HOME/anaconda3/etc/profile.d/conda.sh"
+elif [ -d "$HOME/miniconda3" ]; then
+    source "$HOME/miniconda3/etc/profile.d/conda.sh"
+else
+    echo "Conda not found in anaconda3 or miniconda3."
+    exit 1
+fi
+
 conda activate vaila
 python3 ~/vaila/vaila.py
