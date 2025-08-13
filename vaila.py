6b58843b3a627bab1dde3547d84b78b80939f4f5
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2025-08-13 10:45:59 -0300

    Scout (#390)
    * scout
    
    * fix jump
    
    * scout_vaila_v1
    
    * scout v2

--- vaila.py
+++ vaila.py
@@ -4,8 +4,8 @@ vaila.py
 ===============================================================================
 Author: Prof. Paulo R. P. Santiago
 Date:  07 October 2024
-Update:  12 August 2025
-Version updated: 0.10.15
+Update:  13 August 2025
+Version updated: 0.10.16
 Python Version: 3.12.11
 
 Description:
@@ -135,7 +135,7 @@ if platform.system() == "Darwin":  # macOS
         pass
 
 text = r"""
-vailá - 12.August.2025 v0.10.15 (Python 3.12.11)
+vailá - 13.August.2025 v0.10.16 (Python 3.12.11)
                                              o
                                 _,  o |\  _,/
                           |  |_/ |  | |/ / |
@@ -234,7 +234,7 @@ class Vaila(tk.Tk):
 
         """
         super().__init__()
-        self.title("vailá - 12.August.2025 v0.10.15 (Python 3.12.11)")
+        self.title("vailá - 13.August.2025 v0.10.16 (Python 3.12.11)")
 
         # Adjust dimensions and layout based on the operating system
         self.set_dimensions_based_on_os()
