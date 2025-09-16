38e70bd348334f0cc6dd09365126499fffc5faa2
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2025-09-15 22:15:55 -0300

    updates filt interp and viewc3d (#414)

--- vaila.py
+++ vaila.py
@@ -4,8 +4,8 @@ vaila.py
 ===============================================================================
 Author: Prof. Paulo R. P. Santiago
 Date:  07 October 2024
-Update:  11 September 2025
-Version updated: 0.11.3
+Update:  15 September 2025
+Version updated: 0.11.4
 Python Version: 3.12.11
 
 Description:
@@ -168,7 +168,7 @@ if platform.system() == "Darwin":  # macOS
         pass
 
 text = r"""
-vailá - 11.September.2025 v0.11.3 (Python 3.12.11)
+vailá - 15.September.2025 v0.11.4 (Python 3.12.11)
                                              o
                                 _,  o |\  _,/
                           |  |_/ |  | |/ / |
@@ -267,7 +267,7 @@ class Vaila(tk.Tk):
 
         """
         super().__init__()
-        self.title("vailá - 11.September.2025 v0.11.3 (Python 3.12.11)")
+        self.title("vailá - 15.September.2025 v0.11.4 (Python 3.12.11)")
 
         # Adjust dimensions and layout based on the operating system
         self.set_dimensions_based_on_os()
