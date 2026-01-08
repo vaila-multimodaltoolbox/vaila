079c55fe1788a5816057b441888da172f41cac9f
paulopreto <paulosantiago@usp.br>
2026-01-08 14:20:30 -0300

    sprint

---
+++ vaila/vailasprint.py
@@ -0,0 +1,41 @@
+"""
+Project: vailá Multimodal Toolbox
+Script: vailasprint.py
+
+Author: Paulo Roberto Pereira Santiago
+Email: paulosantiago@usp.br
+GitHub: https://github.com/vaila-multimodaltoolbox/vaila
+Creation Date: 08 January 2026
+Update Date: 08 January 2026
+Version: 0.0.1
+
+Example of usage:
+First activate the vaila environment:
+source .venv/bin/activate
+Then run the vailasprint.py script:
+uv run vailasprint.py -i input_csv_file -o output_directory
+
+Description:
+Analysis of sprint data from CSV file of 2D kinematic video in vailá Tracker YOLO format (105x68 meters).
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def run_vailasprint():
+    """
+    Main function to run the Sprint analysis tool.
+    
+    This function will be called when the script is executed.
+    Placeholder for future development.
+    """
+    print("Hello Sprint!")
+    print("Sprint analysis tool - Under development")
+    # TODO: Add sprint analysis functionality here
+
+
+if __name__ == "__main__":
+    run_vailasprint()
