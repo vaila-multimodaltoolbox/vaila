93f6d6a0e8daaf4323992f246d5df5ec484b2f27
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2026-01-27 06:50:45 -0300

    MediaPipe update, Refine markerless2d v2 pipeline, add configurable filters, and update vailá docs/UI (#475)
    MediaPipe update, Refine markerless2d v2 pipeline, add configurable filters, and update vailá docs/UI

--- vaila/__init__.py
+++ vaila/__init__.py
@@ -6,11 +6,10 @@ Description:
 This is the initialization script for the vaila multimodal toolbox, which serves as the entry point for all modules and functions within the package. This file aggregates all the essential functionalities and provides a streamlined interface for importing and using the various tools available in the package.
 
 Version: 2024-08-16 12:00:00
-Update: 11 January 2026
+Update: 27 January 2026
 
 Changelog:
-- 2024-08-16: Added `process_videos_gui` functionality for video processing. This update integrates the video processing GUI into the package, allowing users to define and concatenate video segments through a user-friendly interface.
-- 2024-10-09: Added `linear_interpolation_split` functionality for applying linear interpolation and data splitting. This update provides enhancing data_processingta cleaning and manipulation features.
+- 2026-01-27: Updated `mediapipe` to version 0.10.32. This update fixes the issue with the `mediapipe` library not being able to find the `mediapipe` module.
 
 Author: Prof. Paulo Santiago
 """
