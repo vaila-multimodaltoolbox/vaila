960121fb88033a12d0cf8c1eedadabc82d2ba7cf
paulopreto <paulosantiago@usp.br>
2026-02-10 22:54:53 -0300

    interp_smooth_split fix

--- vaila.py
+++ vaila.py
@@ -6,8 +6,8 @@ Author: Paulo Roberto Pereira Santiago
 Email: paulosantiago@usp.br
 GitHub: https://github.com/vaila-multimodaltoolbox/vaila
 Creation Date: 07 October 2024
-Update Date: 10 February 2026
-Version: 0.3.20
+Update Date: 11 February 2026
+Version: 0.3.21
 
 Example of usage:
 uv run vaila.py (recommended)
@@ -155,7 +155,7 @@ if platform.system() == "Darwin":  # macOS
         pass
 
 text = r"""
-vailá - 10.February.2026 v0.3.20 (Python 3.12.12)
+vailá - 11.February.2026 v0.3.21 (Python 3.12.12)
                                              o
                                 _,  o |\  _,/
                           |  |_/ |  | |/ / |
@@ -261,7 +261,7 @@ class Vaila(tk.Tk):
 
         """
         super().__init__()
-        self.title("vailá - 10.February.2026 v0.3.20 (Python 3.12.12)")
+        self.title("vailá - 11.February.2026 v0.3.21 (Python 3.12.12)")
 
         # Set WM_CLASS for Linux (needed for proper icon in taskbar/dock)
         if platform.system() == "Linux":
