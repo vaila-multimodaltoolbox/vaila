d0613ff78e51ade8af690b3da3266a407fcc13f8
paulopreto <paulosantiago@usp.br>
2026-04-07 15:01:27 -0300

    new sam

--- vaila.py
+++ vaila.py
@@ -6,8 +6,8 @@ Author: Paulo Roberto Pereira Santiago
 Email: paulosantiago@usp.br
 GitHub: https://github.com/vaila-multimodaltoolbox/vaila
 Creation Date: 07 October 2024
-Update Date: 28 March 2026
-Version: 0.3.33
+Update Date: 07 April 2026
+Version: 0.3.34
 
 Example of usage:
 uv run vaila.py (recommended)
@@ -158,7 +158,7 @@ if platform.system() == "Darwin":  # macOS
         pass
 
 text = r"""
-vailá - 28.March.2026 v0.3.33 (Python 3.12.13)
+vailá - 07.April.2026 v0.3.34 (Python 3.12.13)
                                              o
                                 _,  o |\  _,/
                           |  |_/ |  | |/ / |
@@ -264,7 +264,7 @@ class Vaila(tk.Tk):
 
         """
         super().__init__(className="vaila")
-        self.title("vailá - 28.March.2026 v0.3.33 (Python 3.12.13)")
+        self.title("vailá - 07.April.2026 v0.3.34 (Python 3.12.13)")
 
         # wm class is set via className above, which results in class "Vaila"
         # This is needed for proper icon association in Linux docks/taskbars
